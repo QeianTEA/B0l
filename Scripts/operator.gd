7289535b7254b93c2e28b66aa81a7702d78dd7e3
Kenan <qei@duck.com>
2024-07-19 01:22:46 +0300

    operator now selectable yesyes

--- Scripts/operator.gd
+++ Scripts/operator.gd
@@ -1,5 +1,46 @@
-extends Node2D
+extends CharacterBody2D
 
-#hello
-#click to select
-#move to clicked section
+@export var selected = false
+#@onready var anim = $AnimationPlayer
+@onready var box = $Selected
+
+
+
+@onready var target = position
+var follow_cursor = false
+@export var Speed = 150
+
+
+var target_node = null
+var home_pos = Vector2.ZERO
+
+func _ready():
+	set_selected(selected)
+	home_pos = self.global_position
+	box.visible = false
+	
+
+func set_selected(value):
+	selected = value
+	box.visible = value
+	
+func _input(event):
+	pass
+
+func _physics_process(delta: float) -> void:
+	
+	if selected:
+		pass
+
+	if position.distance_to(target) > 30:
+		move_and_slide()
+	else:
+		#anim.stop()
+		pass
+
+func State(nunmber):
+	match(nunmber):
+		1:
+			print("Su iç")
+		2:
+			print("Su içme")
