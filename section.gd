37bce5c7c1997062ee41897ab0d081415d38ccfe
Kenan <qei@duck.com>
2024-07-29 23:40:43 +0300

    ok godot saolsun. Operator states ready to ROCK

--- section.gd
+++ section.gd
@@ -2,27 +2,28 @@ extends Area2D
 
 @export var MaxOperators = 2
 
-
 @onready var operatorNumber = 0
 @onready var enemyPresent = false
 @onready var damaged = true
 
+
+
+
 func _on_body_entered(body):
 	if body.is_in_group("Operators"):
 		
-		var op = body.get_script()
-		
-		get_tree().call_group("Operators", "SectionEntered")
 		operatorNumber += 1
 		
 		if enemyPresent:
-			op.attack = true
+			body.attack = true
 		elif  damaged:
-			op.repair = true
+			body.repair = true
 		elif operatorNumber <= MaxOperators:
-			op.full = true
+			body.full = true
 		else:
 			pass
+		
+		body.SectionEntered()
 
 
 
@@ -30,6 +31,6 @@ func _on_body_exited(body):
 	if body.is_in_group("Operators"):
 		var op = body.get_script()
 		operatorNumber -= 1
-		op.full = false
-		op.repair = false
-		op.attack = false
+		body.full = false
+		body.repair = false
+		body.attack = false
