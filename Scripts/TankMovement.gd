159f5fd7fa0d9e45ae43b7a45ff0ccda86224ca6
I-rem <ayseirem.kayacioglu@gmail.com>
2025-03-14 14:40:03 +0300

    Add multiple wheels to tank

--- Scripts/TankMovement.gd
+++ Scripts/TankMovement.gd
@@ -1,11 +1,13 @@
 extends RigidBody2D
 
-@export var left_wheel: RigidBody2D
-@export var right_wheel: RigidBody2D
+@export var wheel1: RigidBody2D
+@export var wheel2: RigidBody2D
+@export var wheel3: RigidBody2D
+@export var wheel4: RigidBody2D
 
 const WHEEL_FORCE = 500
-const MAX_SPEED = 200
-const TORQUE_FORCE = 100
+const MAX_SPEED = 800
+const TORQUE_FORCE = 300
 
 func _physics_process(delta):
 	var direction = 0
@@ -17,12 +19,18 @@ func _physics_process(delta):
 
 	if direction != 0:
 		var force = Vector2(direction * WHEEL_FORCE, 0)
-		left_wheel.apply_force(force)
-		right_wheel.apply_force(force)
+		wheel1.apply_force(force)
+		wheel2.apply_force(force)
+		wheel3.apply_force(force)
+		wheel4.apply_force(force)
 	
 		apply_torque(direction * -TORQUE_FORCE)
 
-	if left_wheel.linear_velocity.length() > MAX_SPEED:
-		left_wheel.linear_velocity = left_wheel.linear_velocity.normalized() * MAX_SPEED
-	if right_wheel.linear_velocity.length() > MAX_SPEED:
-		right_wheel.linear_velocity = right_wheel.linear_velocity.normalized() * MAX_SPEED
+	if wheel1.linear_velocity.length() > MAX_SPEED:
+		wheel2.linear_velocity = wheel1.linear_velocity.normalized() * MAX_SPEED
+	if wheel2.linear_velocity.length() > MAX_SPEED:
+		wheel2.linear_velocity = wheel2.linear_velocity.normalized() * MAX_SPEED
+	if wheel3.linear_velocity.length() > MAX_SPEED:
+		wheel3.linear_velocity = wheel3.linear_velocity.normalized() * MAX_SPEED
+	if wheel4.linear_velocity.length() > MAX_SPEED:
+		wheel4.linear_velocity = wheel4.linear_velocity.normalized() * MAX_SPEED
